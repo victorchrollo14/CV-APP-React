08276ebf7930059c7a1ae23e5cee7c9f840f1f9d
Victor <chrollolucilfer1402@gmail.com>
2023-05-17 13:21:36 +0530

    Initial commitset up fonts and typo

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import "./style.css"
+import { Header } from './Header'
+
+function App() {
+
+  return (
+    <>
+      <Header />
+    </>
+  )
+}
+
+export default App
---
+++ src/Header.jsx
@@ -0,0 +1,9 @@
+export function Header() {
+    return (
+        <>
+        <header className="header">
+            <h1 >CV Builder</h1>
+        </header>
+        </>
+    )
+}
